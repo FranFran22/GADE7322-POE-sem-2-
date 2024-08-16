c60adb81f1b11bca59c91476762f950c8d4e81a6
Fran <ArgentumArcus.12@gmail.com>
2024-08-16 22:55:09 +0200

    changes to GameManager script

--- GADE7322 Project (sem 2)/Assets/Scripts/Units/Enemy.cs
+++ GADE7322 Project (sem 2)/Assets/Scripts/Units/Enemy.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 public class Enemy : Unit
 {
-    [SerializeField]
-    private GameObject[] spawnPoints;
-
     public Enemy()
     {
         health = 100;
         damage = 50;
         speed = 15f;
-        spawn = RandomPoint(spawnPoints);
-    }
-
-    private GameObject RandomPoint(GameObject[] array)
-    {
-        int i = UnityEngine.Random.Range(0, array.Length);
-        return array[i];
+        spawn = null;
     }
 }
