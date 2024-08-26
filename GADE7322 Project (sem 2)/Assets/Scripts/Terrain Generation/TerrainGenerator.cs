0eb8be63b96d651584bdd93915efa6d468a3c752
Fran <ArgentumArcus.12@gmail.com>
2024-08-26 16:06:08 +0200

    changes to scripts
    - added Noise() method
    - added Generate() method

--- GADE7322 Project (sem 2)/Assets/Scripts/Terrain Generation/TerrainGenerator.cs
+++ GADE7322 Project (sem 2)/Assets/Scripts/Terrain Generation/TerrainGenerator.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
@@ -19,21 +21,24 @@ public class TerrainGenerator : MonoBehaviour
     // unity3D mesh --> MeshFilter (on a GameObject) --> Meshrenderer
     // heightmap source is a function
 
-    [SerializeField]
-    private float maxValue; //--> noise value
-    [SerializeField]
-    private float minValue; //--> noise value
 
-    private float[,] noiseArray = new float[50,50];
-    private Vector3[] newVertices;
-    private Vector2[] newUV;
-    private int[] newTriangles;
+    private float maxValue = 1f;
+    private float minValue = 0.1f;
+    private static int size = 100;
+
+    private float[,] noiseArray = new float[size, size];
+    private Vector3[] newVertices = new Vector3[size * size];
+    private Vector2[] newUV = new Vector2[size * size];
+    private int[] newTriangles = new int[(size - 1) * (size - 1) * 2 * 3];
+    private int index;
+    private int scalingFactor = 10;
 
 
 
     void Start()
     {
         NoiseGeneration();
+        GenerateMesh();
     }
 
 
@@ -42,27 +47,73 @@ public class TerrainGenerator : MonoBehaviour
         
     }
 
+
     private void NoiseGeneration()
     {
-        for (int i = 0; i < noiseArray.Length; i++)
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < noiseArray.Length; j++)
+            for (int j = 0; j < size; j++)
             {
-                float sample = Mathf.PerlinNoise(maxValue, minValue);
-                noiseArray[i, j] = sample;
+                noiseArray[i, j] = Noise();
             }
         }
     }
 
-    private Mesh GenerateMesh()
+
+    private float Noise()
+    {
+        float frequency = Random.Range(0, 9);
+        frequency = frequency / 10;
+
+        float noiseVal = Mathf.PerlinNoise(maxValue * frequency, minValue * frequency);
+
+        return noiseVal;
+    }
+
+
+    private void GenerateMesh()
     {
         Mesh terrain = new Mesh();
-        GetComponent<MeshFilter>().mesh = terrain;
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh = terrain;
+
+        Generate();
 
         terrain.vertices = newVertices;
         terrain.uv = newUV;
         terrain.triangles = newTriangles;
+    }
 
-        return terrain;
+
+    private void Generate()
+    {
+        index = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                newVertices[index++] = new Vector3(i, noiseArray[i, j] * scalingFactor, j);
+                newUV[index++] = new Vector2(i / (size - 1), j / (size - 1));
+            }
+        }
+
+        index = 0;
+
+        for (int x = 0; x < size - 1; x++)
+        {
+            for (int y = 0; y < size - 1; y++)
+            {
+                // triangle #1
+                newTriangles[index++] = x + y * size;
+                newTriangles[index++] = x + 1 + y * size;
+                newTriangles[index++] = x + 1 + (y + 1) * size;
+
+                // triangle #2
+                newTriangles[index++] = x + y * size;
+                newTriangles[index++] = x + 1 + (y + 1) * size;
+                newTriangles[index++] = x + (y + 1) * size;
+            }
+        }
     }
 }
